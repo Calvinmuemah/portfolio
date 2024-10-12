918d9bf15bae710aa340c2c3ce0ee3e9ad12284f
Calvinmuemah <kelvinmuemah855@gmail.com>
2024-10-12 13:33:06 -0700

    Initial commit

---
+++ script.js
@@ -0,0 +1,7 @@
+// typing animation
+var typed = new typed(".typing",{
+    Strings:["Web Designer","Web Developer","Graphic Designer","YouTuber"],
+    typeSpeed:100,
+    backSpeed:60,
+    loop:true
+})
---
+++ style-switcher.js
@@ -0,0 +1,43 @@
+// toggle style switcher
+const styleSwitcherToggle = document.querySelector(".style-switcher-toggler");
+styleSwitcherToggle.addEventListener("click", () => {
+    document.querySelector(".style-switcher").classList.toggle(".open");
+})
+//hide style - switche on scroll
+window.addEventListener("scroll",()=> {
+    if(document.querySelector(".style-switcher").classList.contains("open"))
+    {
+        document.querySelector(".style-switcher").classList.remove.("open"); 
+    }
+})
+//Theme colors
+const AlternateStyles = document.querySelectorAl(".alternate-style");
+function setActiveStyle(color){
+    alternatestyles.forEach((style) => {
+        if(color === style.getAttribute("title"))
+        {
+            style.removeAttribute("disabled");
+        }
+        else
+        {
+            style.setAttribute("disabled","true");
+        }    
+    })
+}
+// theme light and dark mode
+const dayNight = document.querySelector(".day-night");
+dayNight.addEventListener("click", () =>{
+    dayNight.querySelector("i").classList.toggle("fa-sun");
+    dayNight.querySelector("i").classList.toggle("fa-sun");
+    document.body.classList.toggle("dark");
+})
+window.addEventListener("load", ()=>{
+    if(document.body.classList.contains("dark"))
+    {
+        dayNight.querySelector("i").classList.add("fa-sun");
+    }
+    else
+    {
+        dayNight.querySelector("i").classList.add("fa-moon");
+    }
+})
